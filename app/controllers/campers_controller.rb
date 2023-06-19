29838bfe945489492028ece18026908c29edc831
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-06-19 14:25:02 -0500

    commit tree 0b83a51de8d636d92bc7645775c1869898395b24

---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,32 @@
+class CampersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
+    def index
+        render json: Camper.all
+    end
+
+    def show
+        camper = Camper.find(params[:id])
+        render json: camper, serializer: CamperWithActivitiesSerializer
+    end
+
+    def create
+        new_camper = Camper.create!(camper_params)
+        render json: new_camper, status: :created
+    end
+
+    private
+
+    def camper_params
+        params.permit(:name, :age)
+    end
+
+    def render_not_found
+        render json: { error: "Camper not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity(invalid)
+        render json: {errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+end
